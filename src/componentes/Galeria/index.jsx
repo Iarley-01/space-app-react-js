124f25690147912c449f6fd5c605786f8c402eff
Iarley-01 <yarleyalves2016@gmail.com>
2024-06-18 12:20:36 -0300

    criando estrutura da galeria

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -1,8 +1,28 @@
+import { styled } from 'styled-components';
+
 import Titulo from '../Titulo';
+import Tags from './Tags';
+import Populares from './Populares';
+
+const GaleriaContainer = styled.div`
+  display: flex;
+`
+
+const SecaoFluida = styled.section`
+  flex-grow: 1;
+`
 
 const Galeria = () => {
   return (
-    <Titulo>Navegue pela galeria</Titulo>
+    <>
+      <Tags />
+      <GaleriaContainer>
+        <SecaoFluida>
+          <Titulo>Navegue pela galeria</Titulo>
+        </SecaoFluida>
+        <Populares />
+      </GaleriaContainer>
+    </>
   );
   
 }
